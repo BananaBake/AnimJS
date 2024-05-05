7a46a4e42daac0519b5328ae8e81cdc0b4190a5c
BananaBake <135561643+BananaBake@users.noreply.github.com>
2024-05-05 19:58:55 +0000

    Update

---
+++ animations.md
@@ -0,0 +1,15 @@
+<h3>Spin in</h3>
+<h3>Spin out</h3>
+<h3>Spin right</h3>
+<h3>Spin left</h3>
+<h3>Slide right</h3>
+<h3>Slide left</h3>
+<h3>Slide up</h3>
+<h3>Slide down</h3>
+<h3>Grow</h3>
+<h3>Shrink</h3>
+<h3>Fade in</h3>
+<h3>Fade out</h3>
+<h3>Glow</h3>
+<h3>Text-glow</h3>
+<h3>Type text</h3>
--- script.js
+++ script.js
@@ -5,4 +5,12 @@ animate("fade-in", "fade-in", 3000, "", "infinite");
 animate("glow", "glow", 3000, "", "infinite");
 animateOnVis("divvy", "fade-in", 3000, "", "forwards");
 animateOnVis("divvy", "grow", 3000, "", "forwards");
-animateOnVis("get-now-text", "typeText", 200, "Get AnimJS now");
+animateOnVis("get-now-text", "type", 300);
+
+function show(id) {
+  document.getElementById(id).hidden = false;
+}
+
+function hide(id) {
+  document.getElementById(id).hidden = true;
+}
